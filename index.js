874954e34994b3ca39a6afe8774b1a50d7b58479
kennyjosef <idowukehinde28@gmail.com>
2021-08-27 07:21:07 -0700

    first commit on this project

---
+++ index.js
@@ -0,0 +1,15 @@
+const language= "java"
+language==="java"
+?console.log("correct")
+:language==="python"
+?console.log("python")
+:console.log("incorrect")
+
+for(let i=1; i<=20; i++){
+    console.log(i)
+}
+
+const arr=["java", "css"];
+const  sliced= arr.slice(1)
+console.log(arr);
+console.log(sliced)
